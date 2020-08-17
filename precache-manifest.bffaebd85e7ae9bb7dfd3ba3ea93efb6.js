c703721a9cc95790cdaee4740e6945f03b5b87c9
mdmoniruzzamannahid <mnnahid32584@gmail.com>
2020-08-17 16:30:50 +0600

    Updates

---
+++ precache-manifest.bffaebd85e7ae9bb7dfd3ba3ea93efb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db43e476bcc3bb709909c35914edb115",
+    "url": "/react-editDelet/index.html"
+  },
+  {
+    "revision": "8c87efe427991458fa37",
+    "url": "/react-editDelet/static/css/main.8499b8db.chunk.css"
+  },
+  {
+    "revision": "ff45ab78d6ff8d3928e7",
+    "url": "/react-editDelet/static/js/2.aacbbde0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-editDelet/static/js/2.aacbbde0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c87efe427991458fa37",
+    "url": "/react-editDelet/static/js/main.3867f5a2.chunk.js"
+  },
+  {
+    "revision": "1436c65c7ee2a35d31ed",
+    "url": "/react-editDelet/static/js/runtime-main.3611156f.js"
+  }
+]);
